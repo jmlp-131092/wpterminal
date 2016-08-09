aa6a56a5fb943c94d34397e2a986d0761c0658af
jmlp131092 <jmlp131092@gmail.com>
2016-08-09 18:30:49 +0100

    more stuff

--- wpterminal/bin/modules/theme_module.py
+++ wpterminal/bin/modules/theme_module.py
@@ -7,6 +7,7 @@ class ThemeModule(AbstractModule):
 			self.paths = paths
 			self.themes_path = self.paths['themes']
 			self.theme_path = self.themes_path
+			self.theme_dir_name = '';
 			return
 
 		def create ( self, line = 0) :
@@ -19,4 +20,36 @@ class ThemeModule(AbstractModule):
 			return
 
 		def run ( self ):
+			return
+
+		def _createStyle ( self ) :
+			_file = self._createFile( self.theme_path, '/style.css', 'w+' )
+			return _file
+
+		def _createThemeDirsAndResources ( self ):
+
+			self.theme_res_path = self._createDir( self.theme_path, '/res' )
+			self.theme_css_path = self._createDir( self.theme_res_path, '/css' )
+			self.theme_images_path = self._createDir( self.theme_res_path, '/images' )
+			self.theme_js_path = self._createDir( self.theme_res_path, '/js' )
+			self.theme_inc_path = self._createDir( self.theme_path, '/inc' )
+
+			self.functions_file = self._createFile(self.theme_path, '/functions.php')
+			self.index_file = self._createFile(self.theme_path, '/index.php')
+			self.sidebar_file = self._createFile(self.theme_path, '/sidebar.php')
+			self.page_file = self._createFile(self.theme_path, '/page.php')
+			self.header_file = self._createFile(self.theme_path, '/header.php')
+			self.footer_file = self._createFile(self.theme_path, '/footer.php')
+			self.single_file = self._createFile(self.theme_path, '/single.php')
+
+		def _ createThemeHeader ( self ):
+			return
+
+		def _createThemeFooter ( self ):
+			return
+
+		def _createThemePage ( self ):
+			return
+
+		def _createThemeSingle ( self ):
 			return
